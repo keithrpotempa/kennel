e12ab04e957a68d4d4c3cb8264c98c58aaa6834f
Keith Potempa <keithrpotempa@gmail.com>
2020-02-21 13:52:09 -0600

    finished ch2 practice

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
+    <React.Fragment>
     <address>
       Visit Us at the Nashville North Location
       <br />
       500 Puppy Way
     </address>
+    </React.Fragment>
   );
 };
 
