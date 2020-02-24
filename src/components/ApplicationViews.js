939a5769ca21cc7839d3f7779d856e22403ebbaa
Keith Potempa <keithrpotempa@gmail.com>
2020-02-24 14:11:23 -0600

    location details added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,6 +6,7 @@ import AnimalDetail from "./animal/AnimalDetail"
 import OwnerList from "./owner/OwnerList"
 import EmployeeList from "./employee/EmployeeList"
 import LocationList from "./location/LocationList"
+import LocationDetail from "./location/LocationDetail"
 
 const ApplicationViews = () => {
   return (
@@ -17,29 +18,20 @@ const ApplicationViews = () => {
           return <Home />;
         }}
       />
-      {/* Make sure you add the `exact` attribute here */}
-      <Route exact path="/animals" render={(props) => {
+      <Route 
+        exact path="/animals" render={(props) => {
         return <AnimalList />
       }} />
       <Route path="/animals/:animalId(\d+)" render={(props) => {
-        // Pass the animalId to the AnimalDetailComponent
         return <AnimalDetail animalId={parseInt(props.match.params.animalId)}/>
       }} />
-
-      {/*
-        This is a new route to handle a URL with the following pattern:
-        http://localhost:3000/animals/1
-
-        It will not handle the following URL because the `(\d+)`
-        matches only numbers after the final slash in the URL
-        http://localhost:3000/animals/jack
-      */}
-      <Route
-        path="/locations"
-        render={props => {
+      <Route 
+        exact path="/locations" render={props => {
           return <LocationList />;
-        }}
-      />
+      }} />
+      <Route path="/locations/:locationId(\d+)" render={(props) => {
+        return <LocationDetail locationId={parseInt(props.match.params.locationId)}/>
+      }} />
       <Route
         path="/owners"
         render={props => {
